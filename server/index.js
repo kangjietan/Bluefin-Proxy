75927cdf2c4cdbf5328984f4e5ebe8350ea1cea9
Kevin Ong <kevinyaohuiong@gmail.com>
2020-02-05 10:47:34 -0800

    updated proxy to handle component routes

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const mainPort = 3000;
 const filePath = path.join(__dirname, '..', 'public');
 
 mainApp.use(express.static(filePath));
-// mainApp.use(express.json());
-// mainApp.use(express.urlencoded({ extended: true }));
 
 mainApp.get('/mortgage-calculator.js', (req, res) => {
   axios.get('http://localhost:4003/dist/bundle.js')
@@ -21,6 +19,20 @@ mainApp.get('/mortgage-calculator.js', (req, res) => {
       console.log('Error fetching from mortgage calculator server');
       res.status(500);
       res.send(err);
+    });
+});
+
+mainApp.get('/calculator:listingId', (req, res) => {
+  const { listingId } = req.params;
+  axios.get(`http://localhost:4003/calculator${listingId}`)
+    .then((info) => {
+      res.status(200);
+      res.send(info.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.end();
     });
 });
 
@@ -37,6 +49,34 @@ mainApp.get('/tour-scheduler.js', (req, res) => {
     });
 });
 
+mainApp.get('/house', (req, res) => {
+  const q = (req.query);
+  const id = q.listingId;
+  axios.get(`http://localhost:3002/house?listingId=${id}`)
+    .then((info) => {
+      res.status(200);
+      res.send(info.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.end();
+    });
+});
+
+mainApp.get('/schedule', (req, res) => {
+  axios.get('http://localhost:3002/schedule')
+    .then((info) => {
+      res.status(200);
+      res.send(info.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.end();
+    });
+});
+
 mainApp.get('/listings-carousel.js', (req, res) => {
   axios.get('http://localhost:4004/dist/bundle.js')
     .then((bundle) => {
@@ -47,6 +87,19 @@ mainApp.get('/listings-carousel.js', (req, res) => {
       console.log('Error fetching from listings carousel server');
       res.status(500);
       res.end(err);
+    });
+});
+
+mainApp.get('/similar-listings', (req, res) => {
+  axios.get('http://localhost:4004/similar-listings')
+    .then((info) => {
+      res.status(200);
+      res.send(info.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.end();
     });
 });
 
